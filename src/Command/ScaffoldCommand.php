047949b82cc800dcd46c9d506c29b30d609c7e3b
Julián Gutiérrez <juliangut@gmail.com>
2026-03-09 22:31:34 +0100

    fix cs

--- src/Command/ScaffoldCommand.php
+++ src/Command/ScaffoldCommand.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * (c) 2016-2025 Julián Gutiérrez <juliangut@gmail.com>
+ * (c) 2016-2026 Julián Gutiérrez <juliangut@gmail.com>
  *
  * @license BSD-3-Clause
  * @link https://github.com/juliangut/docker-phpdev
